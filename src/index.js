eb0694acc15fb40d0d9b0970ca88cb01d7d5758a
mohit <mohitleo9@gmail.com>
2017-08-20 09:35:45 -0400

    insert html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-// import txt from './TheJungleBookMetaData.txt';
+import txt from './TheJungleBookMetaData.txt';
 // console.log(txt);
 console.log('hello');
+
+// main
+function main(){
+    $(function(){
+        $('.text-container').text(txt);
+        console.log(txt);
+    });
+}
+
+// run main()
+main();
+
